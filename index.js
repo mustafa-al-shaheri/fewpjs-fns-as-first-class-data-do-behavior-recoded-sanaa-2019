f2533e70c80b211e84554d6b6e1760770fc7ed05
Mustafa Al shaheri <mustafa.alshaheri@gmail.com>
2019-11-02 22:13:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,16 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let string=timeString.split(":");
+  let integers=persenInt(string);
+  if(integers<12){
+    return "Good Morning";
+  }else if (integers>12 && integers<17) {
+    return "Good Afternoon";
+  }else if (integers>17) {
+    return "Good Evening";
+    
+  }
+}
 /* Write your implementation of displayMessage() */
